c3e8bde99637a05b34aa2d6db7b7fd54aa374f57
bradage <bradage.merrill@gmail.com>
2020-09-19 12:09:59 -0600

    final touches for the week

--- js/lastModDt.js
+++
@@ -1,8 +0,0 @@
-  const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-  };
-  var lastModDate = Date(document.lastModified);
-  document.getElementById('LastMod').textContent = lastModDate.toLocalDateString('en-US', options);
---
+++ js/script.js
@@ -0,0 +1,8 @@
+const options = {
+  weekday: 'long',
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric'
+};
+
+document.getElementById('lastMod').textContent = new Date(document.lastModified).toLocaleDateString('en-US', options);
