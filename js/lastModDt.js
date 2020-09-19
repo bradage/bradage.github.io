b89d1b5dce336036a26d1d9c809ffebab2d2b184
bradage <bradage.merrill@gmail.com>
2020-09-18 21:44:41 -0600

    updated formating

--- js/lastModDt.js
+++ js/lastModDt.js
@@ -1,3 +1,8 @@
-  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  };
   var lastModDate = Date(document.lastModified);
   document.getElementById('LastMod').textContent = lastModDate.toLocalDateString('en-US', options);
