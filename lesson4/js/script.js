e2437c7c233b5547908be2d137871bf43e62a424
bradage <bradage.merrill@gmail.com>
2020-10-10 19:45:41 -0600

    js format date update.

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,8 +1,35 @@
-const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-  };
-  
-  document.getElementById('lastMod').textContent = new Date(document.lastModified).toLocaleDateString('en-US', options);
+let days = [
+    "Sunday"
+  , "Monday"
+  , "Tuesday"
+  , "Wednesday"
+  , "Thursday"
+  , "Friday"
+  , "Saturday"
+  , "Sunday"
+]
+
+let months = [
+    "January"
+  , "February"
+  , "March"
+  , "April"
+  , "May"
+  , "June"
+  , "July"
+  , "August"
+  , "September"
+  , "October"
+  , "November"
+  , "December"
+]
+
+let lastMod = new Date(document.lastModified);
+let day = lastMod.getDay();
+let dayOfWeek = days[lastMod.getDay()];
+let month = months[lastMod.getMonth()];
+let year = lastMod.getFullYear();
+
+let formattedDate = dayOfWeek + ", " + day + " " + month + " " + year;
+
+document.getElementById('lastMod').textContent = formattedDate;
