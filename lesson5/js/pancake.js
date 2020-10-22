00cdf63fc2f7748a9f2551afcf4b130c5e375d9a
bradage <bradage.merrill@gmail.com>
2020-10-22 17:42:23 -0600

    setup Week6

---
+++ lesson5/js/pancake.js
@@ -0,0 +1,7 @@
+//pancake banner
+let today = new Date();  
+
+if(today.getDay() == 5)
+{
+  document.getElementById('bannerPancakes').innerHTML = document.writeln("Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.");  
+}
