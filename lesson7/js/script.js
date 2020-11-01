86a6c9a74e2097cd4f226e3239e77c3fcf24c8be
bradage <bradage.merrill@gmail.com>
2020-10-31 23:35:15 -0600

    ready or not here it is.

---
+++ lesson7/js/script.js
@@ -0,0 +1,53 @@
+//Format Date
+//create array to match 0-6 to day of week spelled out
+let days = [
+    "Sunday"
+  , "Monday"
+  , "Tuesday"
+  , "Wednesday"
+  , "Thursday"
+  , "Friday"
+  , "Saturday"
+  , "Sunday"
+]
+
+//create array to match 0-11 to month of year spelled out
+let months = [
+    "January"
+  , "February"
+  , "March"
+  , "April"
+  , "May"
+  , "June"
+  , "July"
+  , "August"
+  , "September"
+  , "October"
+  , "November"
+  , "December"
+]
+
+
+let lastMod = new Date(document.lastModified);
+let day = lastMod.getDay();
+let dayOfWeek = days[lastMod.getDay()];
+let month = months[lastMod.getMonth()];
+let year = lastMod.getFullYear();
+
+let formattedDate = dayOfWeek + ", " + day + " " + month + " " + year;
+
+document.getElementById('lastMod').textContent = formattedDate;
+
+//toggleMenu function
+function toggleMenu() {
+  document.getElementsByClassName("nav")[0].classList.toggle("responsive");
+}
+
+//WebFont
+WebFont.load({
+  google: {
+    families: [
+       'Roboto', 'Hind', 'Sawarabi'
+    ]
+  }
+});
