c1f7e1f14aec5ab6fd7020ebcb8245339c385391
bradage <bradage.merrill@gmail.com>
2020-11-12 07:59:47 -0700

    large complete. working on js class creation.

--- lesson9/js/townData.js
+++ lesson9/js/townData.js
@@ -9,29 +9,35 @@ fetch(requestURL)
     //console.table(jsonObject); // temporary checking for valid response and data parsing
     const towns = jsonObject['towns'];
     for (let i = 0; i < towns.length; i++) {
-      let town = document.createElement('section');
-      let h2 = document.createElement('h2');
-      let motto = document.createElement('p');
-      let founded = document.createElement('p');
-      let pop = document.createElement('p');
-      let avgRain = document.createElement('p');
-      let img = document.createElement('img');
+      if (towns[i].name == "Fish Haven" || towns[i].name == "Preston" || towns[i].name == "Soda Springs") {
+        let town = document.createElement('section');
+        let info = document.createElement('div');
+        info.className = 'info';
+        let h2 = document.createElement('h2');
+        let motto = document.createElement('h4');
+        let founded = document.createElement('p');
+        let pop = document.createElement('p');
+        let avgRain = document.createElement('p');
+        let img = document.createElement('img');
 
-      h2.textContent = towns[i].name;
-      motto.textContent = towns[i].motto;
-      founded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-      pop.textContent = 'Population: ' + towns[i].currentPopulation;
-      avgRain.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
-      img.setAttribute('src', towns[i].photo);
-      img.setAttribute('alt', towns[i].name);
+        
+        h2.textContent = towns[i].name;
+        motto.textContent = towns[i].motto;
+        founded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+        pop.textContent = 'Population: ' + towns[i].currentPopulation;
+        avgRain.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+        img.setAttribute('src', 'images/' + towns[i].photo);
+        img.setAttribute('alt', towns[i].name);
+        
+        town.appendChild(info);
+        town.appendChild(h2);
+        town.appendChild(motto);
+        town.appendChild(founded);
+        town.appendChild(pop);
+        town.appendChild(avgRain);
+        town.appendChild(img);
 
-      town.appendChild(h2);
-      town.appendChild(motto);
-      town.appendChild(founded);
-      town.appendChild(pop);
-      town.appendChild(avgRain);
-      town.appendChild(img);
-
-      document.querySelector('div.towns').appendChild(town);
+        document.querySelector('div.towns').appendChild(town);
+      }
     }
   });
