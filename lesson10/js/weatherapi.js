c911b99250cec379db4b3027f2178f3e7f57ebfd
bradage <bradage.merrill@gmail.com>
2020-11-18 06:05:31 -0700

    weather Summary compelte.

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -4,15 +4,17 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     // console.log(jsObject);
-    //document.getElementById('current-temp').textContent = parseFloat(jsObject.main.temp).toFixed(1);
+    document.getElementById('current-temp').textContent = parseFloat(jsObject.main.temp).toFixed(1);
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
     const desc = jsObject.weather[0].description; // note how we reference the weather array
     //document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
     document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
     document.getElementById('icon').setAttribute('alt', desc);
+    document.getElementById('icon').setAttribute('title', desc);
     document.getElementById('humidity').textContent = jsObject.main.humidity;    
     document.getElementById('wspeed').textContent = parseFloat(jsObject.wind.speed).toFixed(1);   
     document.getElementById('hi-temp').textContent = parseFloat(jsObject.main.temp_max).toFixed(1);
+    document.getElementById('low-temp').textContent = parseFloat(jsObject.main.temp_min).toFixed(1);
 
     //gets input of temperature and wind speed
     //display "N/A" if inputs don't meet requirements
@@ -29,7 +31,7 @@ fetch(apiURL)
       wchill = "N/A";
     }
 
-    document.getElementById('windchill').innerHTML = parseFloat(wchill).toFixed(1);
+    document.getElementById('windchill').textContent = parseFloat(wchill).toFixed(1);
 
     //windhcill function that calulcates winchill from temp and speed.
     function windChill(tempF, speed) {
