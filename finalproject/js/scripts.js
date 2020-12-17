8f7512a83a55931cfecc32aaec6d096761c95f52
bradage <bradage.merrill@gmail.com>
2020-12-16 22:29:45 -0700

    upt to tours and still too far to go.

--- finalproject/js/scripts.js
+++ finalproject/js/scripts.js
@@ -11,3 +11,11 @@ WebFont.load({
 function toggleMenu() {
   document.getElementsByClassName("nav")[0].classList.toggle("responsive");
 }
+
+//Dropdown selection
+function selection() {
+  const s = document.querySelector('#selected');
+  const sel = document.querySelector('#rentalType');
+  s.style.diplay = "block";
+  s, textContent = sel.value;
+}
